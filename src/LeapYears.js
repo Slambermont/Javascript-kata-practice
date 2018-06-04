c5424170ce453504b30cfc16fefef701019551f7
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-04 09:57:46 +0100

    Fix style

--- src/LeapYears.js
+++ src/LeapYears.js
@@ -1,5 +1,5 @@
-function leap(year){
-    if(year % 4 === 0) {
+function leap(year) {
+  if (year % 4 === 0) {
     return (year % 100 === 0 && year % 400 !== 0) ? false : true
   } else {
     return false
