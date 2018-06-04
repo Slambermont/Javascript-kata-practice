1b8b45699309305a6528ff8db61747898f1a1ac0
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-04 09:53:27 +0100

    Not a leap year is divisible by 400

--- spec/LeapYearsSpec.js
+++ spec/LeapYearsSpec.js
@@ -6,4 +6,12 @@ describe('#Leap', function() {
   it('is not a leap year if not divisible by 4', function() {
     expect(leap(3)).toEqual(false)
   });
+
+  it('it not a leap year if divisible by 100', function() {
+    expect(leap(200)).toEqual(false)
+  })
+
+  it('it is a leap year if divisible by 400', function() {
+    expect(leap(400)).toEqual(true)
+  })
 });
--- src/LeapYears.js
+++ src/LeapYears.js
@@ -1,6 +1,10 @@
 function leap(year){
     if(year % 4 === 0) {
-    return true
+    if (year % 100 === 0 && year % 400 !== 0) {
+      return false
+    } else {
+      return true
+    }
   } else {
     return false
   }
