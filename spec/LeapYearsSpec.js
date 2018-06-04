cfe7af9eb2d429b4268c806a1f26cefbd4cfd09b
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-04 09:50:35 +0100

    Leap year is divisible by 4

--- spec/LeapYearsSpec.js
+++ spec/LeapYearsSpec.js
@@ -1,1 +1,9 @@
+describe('#Leap', function() {
+  it('is a leap year if divisible by 4', function() {
+    expect(leap(4)).toEqual(true)
+  });
 
+  it('is not a leap year if not divisible by 4', function() {
+    expect(leap(3)).toEqual(false)
+  });
+});
--- src/LeapYears.js
+++ src/LeapYears.js
@@ -1,1 +1,7 @@
-
+function leap(year){
+    if(year % 4 === 0) {
+    return true
+  } else {
+    return false
+  }
+}
